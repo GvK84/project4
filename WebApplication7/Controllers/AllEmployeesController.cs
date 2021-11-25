4e3a71f2afb48e34adf6899ccb55d8fcddcedb3f
GvK84 <gvkapetanaki@gmail.com>
2021-11-25 17:12:04 +0200

    Add project files.

---
+++ WebApplication7/Controllers/AllEmployeesController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Threading.Tasks;
+using System.Web;
+using ClassLibraryEmp;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using static ClassLibraryEmp.Repos;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication7.Controllers
+{
+    [Route("/[controller]")]
+    public class AllEmployeesController : ControllerBase
+    {
+
+        private List<Employee> GetEmps()
+        {
+            Repos rep = new Repos();
+            List<Employee> allemps = new List<Employee>();
+            foreach (Employee emp in rep.GetAllEmployees())
+            {
+                allemps.Add(emp);
+            }
+            return allemps;
+
+        }
+
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            List<Employee> allemps = GetEmps();
+            return Ok(allemps);
+        }
+
+
+    }
+}
---
+++ WebApplication7/Controllers/EmployeeController.cs
@@ -0,0 +1,44 @@
+﻿using ClassLibraryEmp;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using static ClassLibraryEmp.Repos;
+
+namespace WebApplication7.Controllers
+{
+
+    [Route("/[controller]")]
+    public class EmployeeController: ControllerBase
+    {
+        private List<Employee> emps = new List<Employee>() { new Employee(){
+
+
+               Name="Elefthrios", Surname="Gianoudakis", Salary=150, StartDate=Convert.ToDateTime("01/08/2008"), EndDate=Convert.ToDateTime("01/08/2013")}
+            
+        };
+
+        
+        
+        
+        
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(emps);
+        }
+
+        //[HttpGet]
+        //public ActionResult GetAll()
+        //{
+        //    List<Employee>allemps = GetEmps();
+        //    return Ok(allemps);
+        //}
+
+
+
+
+    }
+}
